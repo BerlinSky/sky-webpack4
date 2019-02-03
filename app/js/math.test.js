d8b71521280d256ad5e1f4cfea863ab1a10ca5d6
Berlin Sky <pedroyoung8989@gmail.com>
2019-02-02 18:18:21 -0800

    add jest

--- app/js/math.test.js
+++ app/js/math.test.js
@@ -1,18 +1,38 @@
 import { sum, mul, sub, div  } from './math';
 // const { sum, mul, sub, div } = require('./math')
 
-test('Adding 1 + 1 equals 2', () => {
-  expect(sum(1, 1)).toBe(2)
+describe('first set', () => {
+  beforeAll(() => {
+    //do something
+  })
+
+  beforeEach(() => {
+    //do something
+  })
+
+  afterAll(() => {
+    //do something
+  })
+
+  afterEach(() => {
+    //do something
+  })
+
+  test('Adding 1 + 1 equals 2', () => {
+    expect(sum(1, 1)).toBe(2)
+  })
+
+  test('Multiplying 1 * 1 equals 1', () => {
+    expect(mul(1, 1)).toBe(1)
+  })
+
+  test('Subtracting 1 - 1 equals 0', () => {
+    expect(sub(1, 1)).toBe(0)
+  })
+
+  test('Dividing 1 / 1 equals 1', () => {
+    expect(div(1, 1)).toBe(1)
+  })
 })
 
-test('Multiplying 1 * 1 equals 1', () => {
-  expect(mul(1, 1)).toBe(1)
-})
 
-test('Subtracting 1 - 1 equals 0', () => {
-  expect(sub(1, 1)).toBe(0)
-})
-
-test('Dividing 1 / 1 equals 1', () => {
-  expect(div(1, 1)).toBe(1)
-})
