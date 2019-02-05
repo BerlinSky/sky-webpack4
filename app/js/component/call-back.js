3fe01ab1f1ce2514cf86b011b43ac15e48d874f3
Berlin Sky <pedroyoung8989@gmail.com>
2019-02-04 16:32:31 -0800

    updates

---
+++ app/js/component/call-back.js
@@ -0,0 +1,3 @@
+export const uppercase = (str, callback) => {
+  callback(str.toUpperCase())
+}
---
+++ app/js/component/call-back.test.js
@@ -0,0 +1,8 @@
+import { uppercase } from "./call-back";
+
+test(`uppercase 'test to equal 'TEST'`, (done) => {
+  uppercase('test', (str)=> {
+    expect(str).toBe('TEST');
+    done();
+  })
+});
