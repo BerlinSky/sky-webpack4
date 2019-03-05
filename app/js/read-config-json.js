c129ae7a5561e483e38dc5ed712f967b639da46c
Berlin Sky <pedroyoung8989@gmail.com>
2019-03-04 17:48:05 -0800

    add fetch

--- app/js/read-config-json.js
+++ app/js/read-config-json.js
@@ -1,24 +1,21 @@
-// import config from "./server-config";
-// import config  from './server-config.json';
-
-var { config } = require('./server-config.json');
-
 export const readConfigInfo = () => {
-  const thisConfig = config;
+  // const thisConfig = config;
 
-  console.log('config', config);
+  // console.log('config', config);
 
-  console.log('thisConfig', thisConfig);
+  // console.log('thisConfig', thisConfig);
 
 
-  fetch('/server-config.json').then(response => {
-    response.json().then(json => {
-      const data = json;
-      console.log("data", data);
-    });
-  });
 
-  return thisConfig;
+  fetch('/server-config.json').then(function (response) {
+    console.log(response.json())
+    }).then(function (json) {
+      console.log('parsed json', json)
+    }).catch(function (ex) {
+      console.log('parsing failed', ex)
+    })
+
+  // return thisConfig;
 }
 
 
