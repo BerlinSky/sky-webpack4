c180ff4f73a008801e8614482d65b4c6a6b4fe34
Berlin Sky <pedroyoung8989@gmail.com>
2019-06-28 19:51:30 -0700

    Set up table

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,5 @@
-// eslint-disable-line no-unused-vars
-import { readConfigInfo } from "./read-config-json";
-
-import $ from 'jquery';
-
 $(function () {
 
-  console.log("landing...");
-
-  const v = "Keine Ahnung!"
-  console.log("landing...", v);
-
-  readConfigInfo();
-
-
+  console.log("test");
 
 });
