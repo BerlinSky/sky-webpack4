17ddb2e26c83062428dcd354e192bd7b8619250a
Berlin Sky <pedroyoung8989@gmail.com>
2019-07-07 20:53:13 -0700

    Add form elements

--- app/js/components/TinyRouter/home.js
+++ app/js/components/TinyRouter/home.js
@@ -1,10 +1,15 @@
 import styles from "./app.module.scss";
-import { formInput } from "./Forms/input-form";
-import { additionalInfo } from "./Forms/additional-input";
-import { userProfile } from "./user-profile";
+import {
+  formInput
+} from "./Forms/input-form";
+import {
+  additionalInfo
+} from "./Forms/additional-input";
+import {
+  userProfile
+} from "./user-profile";
 
-const homeCompMap = [
-  {
+const homeCompMap = [{
     "id": "1000",
     "composer": formInput
   },
@@ -15,14 +20,13 @@ const homeCompMap = [
 ];
 
 const renderedComponents = userProfile.components.map((compId) => {
-  if (compId === "1000" || compId === "2000" ) {
-    const item = homeCompMap.find((comp) => comp.id === compId)
+  const item = homeCompMap.find((comp) => comp.id === compId)
+  if (item) {
     return item.composer;
-  }
-  else {
+  } else {
     return "";
   }
-}).reduce((html , item) => html + item, "");
+}).reduce((html, item) => html + item, "");
 
 export const homePage = () => {
   return `
@@ -31,4 +35,3 @@ export const homePage = () => {
   </div>
   `
 }
-
