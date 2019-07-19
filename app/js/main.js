76f893daf2d1715a9b85672bd47cfa89c6e43b89
Berlin Sky <pedroyoung8989@gmail.com>
2019-07-18 20:53:03 -0700

    add progress wheel

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,6 @@
-import { homePage } from "./components/TinyRouter/home"
-import { noAccessPage } from "./components/TinyRouter/no-access"
-import { userProfile } from "./components/TinyRouter/user-profile";
 
-const contentDiv = document.getElementById('content');
+// const contentDiv = document.getElementById('content');
 
-function home() {
-  contentDiv.innerHTML = homePage();
-}
 
-function noAccess() {
-  contentDiv.innerHTML = noAccessPage();
-}
 
-const loadLandingPage = (profile) => {
-  profile.access ? home() : noAccess();
-}
-
-document.addEventListener("DOMContentLoaded",  loadLandingPage(userProfile))
+// document.addEventListener("DOMContentLoaded",  loadLandingPage(userProfile))
